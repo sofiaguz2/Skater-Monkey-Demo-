05315d08f9e04070960ded309309000552f35c2a
patri <patricia.mareg@gmail.com>
2022-07-27 09:32:35 +0200

    version 3

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-window.onload = () => Game.init()
+window.onload = () => {
+    const button = document.querySelector("#start-button");
+    const fondo =  document.querySelector("#fondo");
+    const canvas =  document.querySelector("#myCanvas");
+    button.onclick = () => {
+        fondo.style.display = "none";
+        canvas.style.display = "block";
+        Game.init()
+    }
+}
